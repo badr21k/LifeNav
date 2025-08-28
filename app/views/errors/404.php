e064c1233dac1f4fa25a74a61da29c19877082b1
Moussa Badr <45165307-badr21k@users.noreply.replit.com>
2025-08-28 17:33:23 +0000

    start point

--- app/views/errors/404.php
+++
@@ -1,3 +0,0 @@
-<?php require 'app/views/templates/header.php'; ?>
-<div class="container mt-4"><div class="alert alert-warning">Not found.</div></div>
-<?php require 'app/views/templates/footer.php'; ?>
